f27fbba7909381eafb87ce505bc6ceabbced00a5
Iffy Eleweke <omaleweks@gmail.com>
2024-03-07 15:47:49 -0500

    Modified 'App.js' to build a basic shopping cart

--- week-1-day-3/shopcart/src/App.js
+++ week-1-day-3/shopcart/src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+// Required Library Components 
+import "./styles.css";
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: [
+        {
+          id: 1,
+          image: "./products/cologne.jpg",
+          desc: "Unisex Cologne",
+          value: 0
+        },
+        {
+          id: 2,
+          image: "./products/iwatch.jpg",
+          desc: "Apple iWatch",
+          value: 0
+        },
+        {
+          id: 3,
+          image: "./products/mug.jpg",
+          desc: "Unique Mug",
+          value: 0
+        },
+        {
+          id: 4,
+          image: "./products/wallet.jpg",
+          desc: "Mens Wallet",
+          value: 0
+        }
+      ]
+    };
+  }
+
+  render() {
+    const { products } = this.state;
+
+    return (
+      <div className="App">
+        <header className="App-header"> 
+          <h1>
+            <FontAwesomeIcon icon={faShoppingCart} /> My Shopping Cart
+          </h1>
+          <div className="product-list">
+            { products.map((product) => (
+              <div key={product.id} className="product-item">
+                <img src={product.image} alt={product.desc} />
+                <div>{product.desc}</div>
+                <div>Value: ${product.value}</div>
+              </div>
+            ))}
+          </div>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
